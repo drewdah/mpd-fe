ed4ba8f9792fe3d2cde19932d66517e082f97b86
Drew Lawton <email@drewlawton.com>
2014-07-30 11:35:53 -0700

    replaced lan ip with localhost

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@ function index(){
 	// Prepare Mopidy instance
 	var mpd = new Mopidy({
 		autoConnect: false,
-		webSocketUrl: "ws://192.168.1.117:6680/mopidy/ws/"
+		webSocketUrl: "ws://localhost:6680/mopidy/ws/"
 	});
 
 	// Event for successful connection
